8adc6f001cb06fd0c8bff0c277081c0890262514
Moise-ALI <77589459+Moise-ALI@users.noreply.github.com>
2021-02-17 12:33:28 +0100

    Add files via upload

---
+++ sara-documentation-composer/sara/cmd.py
@@ -0,0 +1,109 @@
+import argparse
+import sys
+import os
+from jinja2 import Environment, FileSystemLoader
+from jinja2.exceptions import TemplateNotFound
+
+import related
+
+from models import Project, Document
+
+
+class Cmd:
+
+    def __init__(self):
+        self.domain_loaded_from_samples=False
+        self._setup_parser()
+        self._env = None
+        self._template = None
+
+    def _setup_parser(self):
+        parser = argparse.ArgumentParser(description="Cmd")
+
+        template_group = parser.add_argument_group("templates")
+        template_group.add_argument("--location", action='append', help="Template folders locations")
+
+        project_group = parser.add_argument_group("definitions")
+        project_group.add_argument("--project",help="Project definition file in yaml format",default=".")
+        project_group.add_argument("--document",help="Documentation definition file in yaml format",default=".")
+
+        parser.add_argument("action", choices=['render','sample'])
+        parser.add_argument("--template", required=True)
+
+        self._parser = parser
+
+
+    def run(self, args):
+        arguments = self.configure(args)
+
+        action_method = getattr(self, arguments.action)
+        return action_method()
+
+    def configure(self, args):
+        """
+        Configure command from arguments
+        :param args:
+        :return:
+        """
+        arguments = self._parser.parse_args(args=args)
+
+        self.configure_template_runtime(arguments)
+        self.configure_domain_objects(arguments)
+
+        return arguments
+
+
+    def configure_template_runtime(self, arguments):
+        self._env = Environment(loader=FileSystemLoader(arguments.location))
+        try:
+            self._template = self._env.get_template(arguments.template)
+        except TemplateNotFound as tnf:
+            print(tnf,arguments.location,arguments.template)
+            raise tnf
+
+
+    def configure_domain_objects(self, arguments):
+        self.domain_loaded_from_samples = ( arguments.action == 'sample' )
+        if self.domain_loaded_from_samples :
+            self._create_samples()
+        else:
+            with open(arguments.project,'r') as file:
+                self.project = related.from_yaml(file,Project)
+                print(self.project)
+
+            with open(arguments.document, 'r') as file:
+                self.document =related.from_yaml(file,Document)
+
+        self.document.configure_from_project(self.project)
+
+    def render(self):
+        args = {
+            'doc': self.document,
+            'project': self.project
+        }
+        result = self._template.render(args)
+        w = open("test.adoc", "w")
+        w.write(result)
+        w.close()
+        os.system("asciidoctor-pdf test.adoc")
+        os.system(".\\test.pdf")
+
+    def sample(self):
+        self.render()
+
+    def _create_samples(self):
+        self.project = Project.sample()
+        print(self.project)
+
+        self.document = Document.sample()
+        print(self.document)
+        print(' - as yaml - ')
+        print(related.to_yaml(self.document))
+
+
+
+
+if __name__ == '__main__':
+    cmd = Cmd()
+    cmd.run(sys.argv[1:])
+
